c134038d9fac0a0bf781d2b8287d9e14ffea8f15
Kaleb Takele <kaleb.takele0810@gmail.com>
2021-09-17 12:32:24 +0300

    more modifications to the gem file

--- db/migrate/20210908110313_location.rb
+++ db/migrate/20210908110313_location.rb
@@ -1,4 +1,10 @@
-class Location < ActiveRecord::Migration[6.1]
+class CreateLocations < ActiveRecord::Migration[6.1]
   def change
+      create_table :locations do |t|
+      t.string :country
+      t.string :region
+      t.string :city
+
+      t.timestamps
   end
 end
