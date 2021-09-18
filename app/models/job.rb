64185622bcb5ecbce98a441a456ff66cdb19de82
Kaleb Takele <kaleb.takele0810@gmail.com>
2021-09-18 12:45:55 +0300

    Job detail page modified

--- app/models/job.rb
+++ app/models/job.rb
@@ -5,6 +5,9 @@ class Job < ApplicationRecord
 	belongs_to :job_category, :optional => true
 	belongs_to :destination, :optional => true
 	belongs_to :user, :optional => true
+	has_rich_text :howtoApply
+	has_rich_text :discription
+	has_rich_text :jobRequirements
 	#validates :Employer_id, :presence => true
 	#validates :is_approved, :presence => true
 	#validates :createdBy_id, :presence => true
